b58c5b353a7ff4b2b271ea69a9928a74e98a682e
hammidoucheasmae <hamidouche.asmae@gmail.com>
2022-06-08 15:13:42 +0100

    add capacitor and add android

---
+++ info-tangier/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.infotangier.app',
+  appName: 'info tangier',
+  webDir: '/dist',
+  bundledWebRuntime: false
+};
+
+export default config;
