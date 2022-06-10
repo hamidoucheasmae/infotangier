3bf2f6ffe07c00f52bf7070c63f3edb17e625e97
hammidoucheasmae <hamidouche.asmae@gmail.com>
2022-06-10 18:14:53 +0100

    add

--- nomade/src/app/app.module.ts
+++ nomade/src/app/app.module.ts
@@ -6,11 +6,34 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+// // firebase staff
+// import { initializeAapp, provideFirebaseApp } from '@angular/fire';
+// import { environment } from '../environments/environment';
+// import { getAuth, provideAuth } from '@angular/fire/auth';
+// import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment.prod';
+import { AngularFirestore } from '@angular/fire/firestore/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { SignInPage } from './sign-in/sign-in.page';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,SignInPage],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    // provideFirebaseApp(()=>initializeAapp(environment.firebase)),
+    // provideAuth(() => getAuth()),
+    // provideFirestore(() => getFirestore()),
+    // provideStorage(() => getStorage()),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+  
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- nomade/src/environments/environment.prod.ts
+++ nomade/src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyASAc1j4Z6bZExSmWxRIHCnQNnyZvnJYck",
-  authDomain: "info-social.firebaseapp.com",
-  projectId: "info-social",
-  storageBucket: "info-social.appspot.com",
-  messagingSenderId: "868415552563",
-  appId: "1:868415552563:web:cbda5c9e19491175737746",
-  measurementId: "G-Q43TPCME55"
+    apiKey: "AIzaSyBGK51rlnDG0M412zjgjGkJ8mV4lL6YKpU",
+    authDomain: "info-tangier.firebaseapp.com",
+    databaseURL: "https://info-tangier-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "info-tangier",
+    storageBucket: "info-tangier.appspot.com",
+    messagingSenderId: "278817344533",
+    appId: "1:278817344533:web:638a89477573893267521c",
+    measurementId: "G-G17G6PWMS1"
   }
 };
