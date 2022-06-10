8a92fce8719c07cb4316a39f9797d75f6d5455a2
hammidoucheasmae <hamidouche.asmae@gmail.com>
2022-06-10 14:30:12 +0100

    save changes

--- info-tangier-admin/README.md
+++ info-tangier-admin/README.md
@@ -1,4 +1,4 @@
-# InfoTangierAdmin
+# InfotangierAdmin
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.5.
 
--- info-tangier-admin/src/environments/environment.ts
+++ info-tangier-admin/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBGK51rlnDG0M412zjgjGkJ8mV4lL6YKpU",
-    authDomain: "info-tangier.firebaseapp.com",
-    projectId: "info-tangier",
-    storageBucket: "info-tangier.appspot.com",
-    messagingSenderId: "278817344533",
-    appId: "1:278817344533:web:638a89477573893267521c",
-    measurementId: "G-G17G6PWMS1"
+  apiKey: "AIzaSyASAc1j4Z6bZExSmWxRIHCnQNnyZvnJYck",
+  authDomain: "info-social.firebaseapp.com",
+  projectId: "info-social",
+  storageBucket: "info-social.appspot.com",
+  messagingSenderId: "868415552563",
+  appId: "1:868415552563:web:cbda5c9e19491175737746",
+  measurementId: "G-Q43TPCME55"
   }
 };
 
