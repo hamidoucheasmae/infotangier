4a128c222794caa0ad416b3b46ee0888d5f74fb8
hammidoucheasmae <hamidouche.asmae@gmail.com>
2022-06-10 14:35:02 +0100

    save changes

---
+++ info-tangier nomade/src/app/pages/single-category/single-category.component.ts
@@ -0,0 +1,28 @@
+import { Category } from './../../../../../info-tangier-admin/src/app/models/category';
+import { PostService } from 'src/app/services/post.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-category',
+  templateUrl: './single-category.component.html',
+  styleUrls: ['./single-category.component.css']
+})
+export class SingleCategoryComponent implements OnInit {
+
+  postArray !: any[];
+  categoryObj !: any;
+
+  constructor(private route: ActivatedRoute, private postService : PostService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(val => {
+      this.categoryObj = val
+
+      this.postService.loadCategoryPost(val['id']).subscribe(post =>{ this.postArray = post;
+
+      })
+    })
+  }
+
+}
---
+++ info-tangier nomade/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyASAc1j4Z6bZExSmWxRIHCnQNnyZvnJYck",
+  authDomain: "info-social.firebaseapp.com",
+  projectId: "info-social",
+  storageBucket: "info-social.appspot.com",
+  messagingSenderId: "868415552563",
+  appId: "1:868415552563:web:cbda5c9e19491175737746",
+  measurementId: "G-Q43TPCME55"
+  }
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
