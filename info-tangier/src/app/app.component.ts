f04489daaafd6bd16903294d34b1a1c2212cf59c
Asmae Hamidouche <76485278+hamidoucheasmae@users.noreply.github.com>
2022-06-14 18:06:32 +0100

    add android

--- info-tangier/src/app/app.component.ts
+++ info-tangier/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { StatusBarInfo } from '@capacitor/status-bar';
 import { Platform } from '@ionic/angular';
 import { FirebaseAuthService } from './pages/welcome/providers/firebase-auth.service';
 import { WidgetUtilService } from './pages/welcome/providers/widget-util.service';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -14,6 +16,7 @@ export class AppComponent {
 
   constructor(
     private platform: Platform, 
+    private statusBar: StatusBar,
     private firebaseAuthService: FirebaseAuthService,
     private router: Router,
     private widgetUtilService: WidgetUtilService
@@ -23,7 +26,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-    
+      this.statusBar.backgroundColorByHexString('#fffff')
     });
     this.getAuthState();
   }
